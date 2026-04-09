718f61f523c2764e2fa2ca5e9dc5adef34121448
Hermann Vargens <96803586+hermannvargens@users.noreply.github.com>
2026-04-09 14:39:45 -0300

    Fix subplot size in app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ alpha = st.sidebar.number_input("Volatilidade Relativa ($\\alpha$)", 1.1, 10.0, 
 x_curva = np.linspace(0, 1, 100)
 y_curva = (alpha * x_curva) / (1 + (alpha - 1) * x_curva)
 
-fig, ax = plt.subplots(figsize=(10, 10))
+fig, ax = plt.subplots(figsize=(6,6)
 ax.plot(x_curva, y_curva, 'b', label='Equilíbrio', linewidth=2)
 ax.plot([0, 1], [0, 1], 'k--', alpha=0.5, label='x = y')
 
